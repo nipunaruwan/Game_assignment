7c9a2c7f212c60503f4d0facd3337bc51c1f1803
mipunaruwan <nipunakalpa1234@gmail.com>
2022-04-03 12:40:00 +0530

    game

---
+++ assert/Script.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    var anim_id;
+
+    //saving dom objects to variables
+    var container = $('#container');
+    var bird = $('#bird');
+    var pole = $('.pole');
+    var pole_1 = $('#pole_1');
+    var pole_2 = $('#pole_2');
+    var restart_btn = $('#restart_btn');
